7c518bba6c29f71a418e79a9b706786ee6d40c0b
Banti Agrawal <banti.agrawal@gmail.com>
2021-03-13 23:00:04 +0100

    initial commit

---
+++ apis/utility.py
@@ -0,0 +1,19 @@
+import tweepy
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+consumer_key = os.getenv("consumer_key")
+consumer_secret = os.getenv("consumer_secret")
+access_token = os.getenv("access_token")
+access_token_secret = os.getenv("access_token_secrete")
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
